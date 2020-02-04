1dee1561aa0081269cf6c8bc0ba8cd1104443aac
fcorra <santandercorradini@gmail.com>
2020-02-04 11:30:58 +0100

    windows dependencies 0

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,13 +1,18 @@
-if(getRversion() < "3.3.0") {
-  stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
-}
+# if(getRversion() < "3.3.0") {
+#   stop("Your version of R is too old. This package requires R-3.3.0 or newer on Windows.")
+# }
+#
+# # Download gdal-2.2.0 from rwinlib
+# VERSION <- commandArgs(TRUE)
+# if(!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))){
+#   if(getRversion() < "3.3.0") setInternet2()
+#   download.file(sprintf("https://github.com/rwinlib/gdal2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+#   dir.create("../windows", showWarnings = FALSE)
+#   unzip("lib.zip", exdir = "../windows")
+#   unlink("lib.zip")
+# }
 
-# Download gdal-2.2.0 from rwinlib
-VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/gdal2-%s/include/gdal/gdal.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/gdal2/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
-  dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
-}
+download.file("https://github.com/OSGeo/gdal/archive/master.zip", destfile = "gdal.zip", quiet = TRUE)
+dir.create("../libgdal", showWarnings = FALSE)
+unzip("gdal.zip", exdir = "..libgdal")
+unlink("gdal.zip")
